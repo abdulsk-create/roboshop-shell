0410837256d26890141e8634ce8a18af6cebe8c7
abdul <abdulsk054@gmail.com>
2023-11-10 10:22:08 +0530

    roboshop services configured

---
+++ mongodb.sh
@@ -0,0 +1,10 @@
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org -y
+
+#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+systemctl enable mongod
+systemctl start mongod
+
+systemctl enable mongod
+systemctl restart mongod
