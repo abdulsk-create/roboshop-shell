df4918ee37aeda64e68ddd28c3d9d407f55d8286
abdul <abdulsk054@gmail.com>
2023-11-19 09:58:36 +0530

    configured roboshop components

--- mysql.sh
+++ mysql.sh
@@ -4,7 +4,7 @@ yum module disable mysql -y
 yum install mysql-community-server -y
 
 systemctl enable mysqld
-systemctl restart mysqld
+systemctl start mysqld
 
 mysql_secure_installation --set-root-pass RoboShop@1
 
