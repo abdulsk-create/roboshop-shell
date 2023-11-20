03846f2d495739533a5412df60ccb8a613a86e83
abdul <abdulsk054@gmail.com>
2023-11-20 10:40:33 +0530

    functions in roboshop project

--- common.sh
+++ common.sh
@@ -1,4 +1,29 @@
-nodejs() {
+func_apppreq() {
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create application user <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  useradd roboshop &>>${log}
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> removing old application content <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  rm -rf /app&>>${log}
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create application directory <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  mkdir /app &>>${log}
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> download application content <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> extract application content <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  cd /app
+  unzip /tmp/${component}.zip &>>${log}
+  cd /app
+}
+func_systemd() {
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> start ${component} service <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
+}
+
+func_nodejs() {
   log=/tmp/roboshop.log
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create ${component} service <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
@@ -12,22 +37,7 @@ nodejs() {
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> install nodejs <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
 
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create application user <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  useradd roboshop &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> removing old content <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  rm -rf /app&>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create application directory <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  mkdir /app &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> download application content <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> extract application content <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  cd /app
-  unzip /tmp/${component}.zip &>>${log}
-  cd /app
+  func_apppreq
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> download nodejs dependencies <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   npm install &>>${log}
@@ -37,9 +47,28 @@ nodejs() {
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> load user schema <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   mongo --host mongodb.entertanova.com </app/schema/${component}.js &>>${log}
+}
 
-  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> start user service <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-  systemctl daemon-reload &>>${log}
-  systemctl enable ${component} &>>${log}
-  systemctl restart ${component} &>>${log}
+func_java() {
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create ${component} service <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> install maven <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  yum install maven -y
+
+  func_apppreq
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> build ${component} service <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> install mysql service client <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  yum install mysql -y
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> load schema <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+  mysql -h mysql.entertanova.com -uroot -pRoboShop@1 < /app/schema/${component}.sql
+
+  echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> build ${component} service <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+
+  func_systemd
 }
