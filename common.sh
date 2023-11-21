f51853409cd979a65fba908ccff8afaedddde9c6
abdul <abdulsk054@gmail.com>
2023-11-21 12:37:23 +0530

    schema setup

--- common.sh
+++ common.sh
@@ -1,45 +1,53 @@
 log=/tmp/roboshop.log
 
+func_exit_status() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[31m FAILURE \e[0m"
+  fi
+}
+
 func_apppreq() {
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create ${component} service <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create application user <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   useradd roboshop &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> removing old application content <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   rm -rf /app&>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create application directory <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   mkdir /app &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> download application content <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> extract application content <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   cd /app
   unzip /tmp/${component}.zip &>>${log}
   cd /app
-  echo $?
+  func_exit_status
 }
 func_systemd() {
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> start ${component} service <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   systemctl daemon-reload &>>${log}
   systemctl enable ${component} &>>${log}
   systemctl restart ${component} &>>${log}
-  echo $?
+  func_exit_status
 }
 
 func_schema_setup() {
   if [ "${schema_type}" == "mongodb" ]; then
     echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> install mongo client <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     yum install mongodb-org-shell -y &>>${log}
-    echo $?
+    func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> load user schema <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     mongo --host mongodb.entertanova.com </app/schema/${component}.js &>>${log}
@@ -48,11 +56,11 @@ func_schema_setup() {
   if [ "${schema_type}" == "mysql" ]; then
     echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> install mysql service client <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     yum install mysql -y &>>${log}
-    echo $?
+    func_exit_status
 
     echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> load schema <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
     mysql -h mysql.entertanova.com -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
-    echo $?
+    func_exit_status
   fi
 }
 
@@ -61,20 +69,20 @@ func_nodejs() {
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> create mongodb repo <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> install nodejs repos <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-  echo $?
+  func_exit_status
 
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> install nodejs <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   yum install nodejs -y &>>${log}
-  echo $?
+  func_exit_status
 
   func_apppreq
   echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>>>> download nodejs dependencies <<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
   npm install &>>${log}
-  echo $?
+  func_exit_status
 
   func_schema_setup
 
